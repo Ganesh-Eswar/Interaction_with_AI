fce00e8c46bc35c4675be920c690b814965f3cf0
Ganesh-Eswar <ganesheswariit@gmail.com>
2023-06-24 23:39:07 +0530

    Interaction with version 1 commit

--- ai_connection.py
+++ ai_connection.py
@@ -1,9 +1,29 @@
 import openai
+
+"""
+Create your API SECRET KEY:
+Visit the OpenAI website: Go to the OpenAI platform website at https://www.openai.com/.
+
+Sign in or sign up: If you already have an account, sign in using your credentials. 
+Otherwise, create a new account by signing up.
+
+Access the API section: Once you're logged in, navigate to the API section of the OpenAI platform. 
+This is where you can manage your API keys.
+
+Generate a new API key: In the API section, look for the option to generate a new API key. 
+Click on it to create a new key specifically for accessing the ChatGPT API.
+
+Store your API key securely: Once the API key is generated, make sure to copy and store it in a safe and secure location. 
+Treat your API key as confidential information and avoid sharing it publicly or committing it to version control repositories.
+
+Configure your API key in your code: Depending on how you plan to use the ChatGPT API, you will need to configure your API key in your code. 
+This typically involves adding the API key as a parameter or environment variable when making API requests. 
+"""
+        
 class AIConnection():
     @staticmethod
     def connect_ai(user_content:str,robot_content:str):
-        openai.api_key = "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"
-
+        openai.api_key = "PASTE YOUR SECRET KEY HERE"
         response = openai.ChatCompletion.create(
             model="gpt-3.5-turbo",
             messages=[
@@ -11,7 +31,6 @@ class AIConnection():
                     {"role": "user", "content": user_content},
                 ]
         )
-
         result = ''
         for choice in response.choices:
             result += choice.message.content
--- chatApp_front.py
+++ chatApp_front.py
@@ -17,26 +17,21 @@ class InteractionContainer():
     def __init__(self,root):
         self.interaction_container = Frame(root)
         self.interaction_container.pack(side="top",fill="both",expand=True)
-        
         self.canvas = Canvas(self.interaction_container)
         self.scrollbar = Scrollbar(self.interaction_container,
-                                                orient='vertical',command=self.canvas.yview)
+                                    orient='vertical',
+                                    command=self.canvas.yview)
         self.scrollable_frame = Frame(self.canvas)
-        
         self.scrollable_frame.bind(
             "<Configure>",
             lambda e: self.canvas.configure(
             scrollregion=self.canvas.bbox("all")
             ))
-        
         self.scrollable_frame.pack(side='left',fill='x',expand=True)
         self.canvas.create_window((0, 0), window=self.scrollable_frame, anchor="nw")
-
         self.canvas.configure(yscrollcommand=self.scrollbar.set)
-        
         self.canvas.pack(side="left", fill="both", expand=True)
         self.scrollbar.pack(side="right", fill="y")
-
         self.AI_image = Image.open(root.robot_img)
         self.AI_image = self.AI_image.resize((40,60))
         self.AIimage = ImageTk.PhotoImage(image=self.AI_image)
@@ -49,51 +44,39 @@ class InteractionContainer():
     def initial_robot_message(self):
         self.init_container = Frame(self.scrollable_frame)
         self.init_container.pack(side="top",fill="x")
-        
-        self.robot_avatar = Label(self.init_container,image=self.AIimage,
-                                  )
+        self.robot_avatar = Label(self.init_container,image=self.AIimage)
         self.robot_avatar.image = self.AIimage
         self.robot_avatar.grid(row=0,column=0,sticky="W")
         space1 = "                                                       "        
         self.robot_initial_msg = Label(self.init_container,text=self.robot_type+(space1*5),
                                        font=("Helvetica",10),
                                        padx=5,pady=10)
-        
         self.robot_initial_msg.grid(row=0,column=1,sticky="W",padx=15,pady=5)
         
     def interaction(self,user_entry,controller):
         self.user_type = user_entry
-        
         self.ai_reply = (AI.AIConnection.connect_ai(self.user_type,self.robot_type))
-        
         self.setting_component()
     
     def setting_component(self):
-        
         self.right_container = Frame(self.scrollable_frame)
         self.right_container.pack(side="top",fill="x")
         self.robot_type = self.ai_reply
-        self.user_avatar = Label(self.right_container,image=self.dimage,
-                                  )
+        self.user_avatar = Label(self.right_container,image=self.dimage)
         self.user_avatar.image = self.dimage
         self.user_avatar.pack(side="right",padx=15,pady=5)
         self.user_msg = Label(self.right_container,text=self.user_type,
                                        font=("Helvetica",10),background="yellow",
                                        padx=5,pady=5,wraplength=400)
         self.user_msg.pack(side="right",padx=15,pady=5)
-        
         self.left_container = Frame(self.scrollable_frame)
         self.left_container.pack(side="top",fill="x")
-        
-        self.robot_avatar = Label(self.left_container,image=self.AIimage,
-                                  )
+        self.robot_avatar = Label(self.left_container,image=self.AIimage)
         self.robot_avatar.image = self.AIimage
         self.robot_avatar.grid(row=0,column=0,sticky="W",padx=15,pady=5)
-        
         self.robot_initial_msg = Label(self.left_container,text=self.robot_type,
                                        font=("Helvetica",10),background="pink",
                                        padx=5,pady=10,wraplength=400)
-        
         self.robot_initial_msg.grid(row=0,column=1,sticky="W",padx=15,pady=5)
         self.canvas.after(150,lambda: self.canvas.yview_moveto(1.0))
 
@@ -103,24 +86,18 @@ class UserEnterContainer():
         self.control = interactionController
         self.bottom_container = Frame(self.root)
         self.bottom_container.pack(side="bottom",pady=40)
-        
         self.developer_image = Image.open(self.root.user_img)
         self.developer_image = self.developer_image.resize((60,60))
         self.dimage = ImageTk.PhotoImage(image=self.developer_image)
-        
         self.user_avatar = Label(self.bottom_container,image=self.dimage)
         self.user_avatar.image = self.dimage
         self.user_avatar.pack(side="left",padx=20)
-         
         self.user_text = Text(self.bottom_container,
                               height=2,width=170,
-                              background="white",
-                              
+                              background="white",                   
                               font=("Helvetica",10,'bold'),padx=5,pady=5)
         self.user_text.insert(INSERT,"Hello AI")
-        
         self.user_text.pack(side="left")
-        
         self.send_button = Button(self.bottom_container,text="Send",
                                   background="blue",foreground="white",
                                   font=("Helvetica",10),padx=10,
@@ -131,10 +108,7 @@ class UserEnterContainer():
         entry = self.user_text.get(1.0,'end')
         self.user_text.delete(1.0,'end')
         self.control.interaction(entry,self.root)
-        
-        
-        
-              
+
 if __name__ == "__main__":
     user_img = r"chatApp\user_img.png"
     robot_img = r"chatApp\robot_img.png" 
